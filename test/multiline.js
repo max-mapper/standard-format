2a047fd02c284e004085d98328ac133b9de03c6b
William Casarin <bill@casarin.me>
2015-02-10 16:28:15 -0800

    standardize

--- test/multiline.js
+++ test/multiline.js
@@ -1,21 +1,20 @@
-
 var test = require('tape')
 var fmt = require('../').transform
 
 test('multiline ', function (t) {
   t.plan(1)
 
-  var program = 
-    "var x = 1\n" +
-    "\n" +
-    "\n" +
-    "\n" +
-    "var z = 2"
+  var program =
+    'var x = 1\n' +
+    '\n' +
+    '\n' +
+    '\n' +
+    'var z = 2'
 
   var expected =
-    "var x = 1\n" +
-    "\n" +
-    "var z = 2"
+    'var x = 1\n' +
+    '\n' +
+    'var z = 2'
 
   var msg = 'two or more empty lines should collapse to one'
   t.equal(fmt(program), expected, msg)
