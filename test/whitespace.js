d1de12563066fbf620fac2fab6b5e649630092dc
Dan Flettre <fletd01@yahoo.com>
2015-02-16 21:48:12 -0600

    more standardizing
    call standard as part of npm test
    add "test.js" to standard.ignore in package.json
    fix a few standard lint issues

--- test/whitespace.js
+++ test/whitespace.js
@@ -1,16 +1,15 @@
-
 var test = require('tape')
 var fmt = require('../').transform
 
 test('sol whitespace', function (t) {
   t.plan(2)
 
-  var program  = '   [1,2,3].map'
+  var program = '   [1,2,3].map'
   var expected = '   ;[1,2,3].map'
   var msg = 'first line sol whitespace is preserved during semicolon insertion'
   t.equal(fmt(program), expected, msg)
 
-  program  = '\n\t\t[1,2,3].map'
+  program = '\n\t\t[1,2,3].map'
   expected = '\n\t\t;[1,2,3].map'
   msg = 'newline sol tabs are preserved during semicolon insertion'
   t.equal(fmt(program), expected, msg)
