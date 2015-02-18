3dba6f8a599b7aa0283d4232967ca8b8a40233a7
William Casarin <bill@casarin.me>
2015-02-17 17:18:44 -0800

    Use esformatter instead of regex

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var fs = require('fs')
 var glob = require('glob')
 var findRoot = require('find-root')
 var Minimatch = require('minimatch').Minimatch
+var formatter = require('esformatter')
 
+var ESFORMATTER_CONFIG = require(path.join(__dirname, 'rc', 'esformatter.json'))
 var DEFAULT_IGNORE = [
   'node_modules/**',
   '.git/**',
@@ -12,31 +14,8 @@ var DEFAULT_IGNORE = [
   '**/bundle.js'
 ]
 
-var NAMED_FUNCTION_NOSPACE = /function(\s+)?(\w+)(\s+)?\(/ig
-var ANON_FUNCTION_NOSPACE = /function(\s+)?\(/ig
-var NAMED_FUNCTION_SPACE = 'function $2 ('
-var ANON_FUNCTION_SPACE = 'function ('
-var FUNCTION_DECLARATION = /function\s(\w+)\s\((.+)?\)(\s+)?\{/ig
-var ANON_FUNCTION_DECLARATION = /function\s\((.+)?\)(\s+)?\{/ig
-var CLEAN_FUNCTION_DECLARATION = 'function $1 ($2) {'
-var CLEAN_ANON_FUNCTION_DECLARATION = 'function ($1) {'
-var MULTI_NEWLINE = /((?:\r?\n){3,})/g
-var EOL_SEMICOLON = /;\r?\n/g
-var SOL_SEMICOLON = /((?:\r?\n|^)[\t ]*)(\(|\[)/g
-var EOL = os.EOL
-var EOL_WHITESPACE = /[\t ]+\r?\n/g
-var SOL_SEMICOLON_BRACE = '$1;$2'
-
 module.exports.transform = function (file) {
-  return file
-    .replace(NAMED_FUNCTION_NOSPACE, NAMED_FUNCTION_SPACE)
-    .replace(ANON_FUNCTION_NOSPACE, ANON_FUNCTION_SPACE)
-    .replace(FUNCTION_DECLARATION, CLEAN_FUNCTION_DECLARATION)
-    .replace(ANON_FUNCTION_DECLARATION, CLEAN_ANON_FUNCTION_DECLARATION)
-    .replace(EOL_SEMICOLON, EOL)
-    .replace(SOL_SEMICOLON, SOL_SEMICOLON_BRACE)
-    .replace(EOL_WHITESPACE, EOL)
-    .replace(MULTI_NEWLINE, EOL + EOL)
+  return formatter.format(file, ESFORMATTER_CONFIG)
 }
 
 module.exports.load = function (opts, cb) {
