3375f395ee08efcface6c8601fe5f405d73684f9
Bret Comnes <bcomnes@gmail.com>
2015-08-21 19:21:46 -0700

    Handle error.

--- test/index.js
+++ test/index.js
@@ -19,6 +19,7 @@ test('test.js formatted and linted without error', function (t) {
     }
 
     standard.lintText(formatted, function (err, result) {
+      t.error(err, 'linting should be error free')
       t.equal(result.errorCount, 0, 'there should be no linting errors after formatting')
       t.equal(result.warningCount, 0, 'there should be no linting warnings after formatting')
       if (result.errorCount || result.warningCount !== 0) console.log(inspect(result, {depth: null}))
