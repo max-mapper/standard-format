547e9c12448967402b6cf28c06ec81ad67fb5c83
Bret Comnes <bcomnes@gmail.com>
2015-11-07 14:43:41 -0800

    Add test based on es6 arrow function  indent issue

--- test/failing/es6.js
+++ test/failing/es6.js
@@ -19,10 +19,32 @@ var classes = [
 
     msg: 'class methods should have correct spacing',
     issues: [
-      "https://github.com/maxogden/standard-format/issues/126",
-      "https://github.com/millermedeiros/esformatter/issues/384",
-      "https://github.com/maxogden/standard-format/issues/111",
-      "https://github.com/maxogden/standard-format/issues/75"
+      'https://github.com/maxogden/standard-format/issues/126',
+      'https://github.com/millermedeiros/esformatter/issues/384',
+      'https://github.com/maxogden/standard-format/issues/111',
+      'https://github.com/maxogden/standard-format/issues/75'
+    ]
+  },
+  {
+    program: [
+      'array.map(el => ({',
+      'inc: el.val + 1,',
+      'dec: el.val - 1',
+      '}))',
+      ''
+    ].join('\n'),
+
+    expected: [
+      'array.map(el => ({',
+      '  inc: el.val + 1,',
+      '  dec: el.val - 1',
+      '}))',
+      ''
+    ].join('\n'),
+
+    msg: 'arrow functions correctly indent',
+    issues: [
+      'https://github.com/maxogden/standard-format/issues/127'
     ]
   }
 ]
